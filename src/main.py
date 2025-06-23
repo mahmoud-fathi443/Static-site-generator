33a43e33941c3a05d81c0b88caee941f66c6118b
mahmoud-fathi443 <mahmoudfathi443@gmail.com>
2025-06-24 00:11:36 +0300

    Public files generation

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ from textnode import *
 from htmlnode import *
 from extract_mk import *
 from split_nodes_delimiter import *
+import os
+import shutil
+
+
 
 def text_to_textnodes(text):
     nodes = [TextNode(text, TextType.NORMAL)]
@@ -12,21 +16,47 @@ def text_to_textnodes(text):
     nodes = split_nodes_link(nodes)
     return nodes
 
+def generat_public_files_recursive(src_dir, dest_dir):
+    for f in os.listdir(src_dir):
+        src_f = os.path.join(src_dir, f)
+        dest_f = os.path.join(dest_dir, f)
+        if os.path.isfile(src_f):
+            shutil.copy(src_f, dest_f)
+        else:
+            os.mkdir(dest_f)
+            generat_public_files_recursive(src_f, dest_f)
+
+
+
     
 
 
-def main():
-    node = ParentNode(
-    "p",
-    [
-        LeafNode("b", "Bold text"),
-        LeafNode(None, "Normal text"),
-        LeafNode("i", "italic text"),
-        LeafNode(None, "Normal text"),
-    ],
-)
 
-    print(node.to_html())
+def generate_public_files(src_dir, dest_dir):
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+
+    os.makedirs(dest_dir)
+
+    generat_public_files_recursive(src_dir, dest_dir)
+
+    print(os.listdir(src_dir))
+    print(os.listdir(dest_dir))
+
+    
+
+
+
+    
+
+
+
+
+def main():
+    generate_public_files("static", "public")
+    
+    
+
 
 
 if __name__ == "__main__":
